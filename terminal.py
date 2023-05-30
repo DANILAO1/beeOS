367031312109dbfdaa4f4334f1d1bb52b3a66a80
DANILAO <91478969+THEspager@users.noreply.github.com>
2023-05-30 13:20:43 +0200

    added folders and setting v1.1a

--- terminal.py
+++ terminal.py
@@ -1,31 +1,61 @@
 #this program is copyrighted by Danila(DANILAO)
 #os isn't finished
 
-version = "v0.11a"
 
 def main():
     username = ""
     choice = ""
+    settingchoice = ""
+    folderschoice = ""
     run = True
-    version = "v0.11a"
+    settings = True
+    folder = True
+    version = "v1.1a"
 
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    #user log
     print("copyrighted by Danila 2023©")
     print("welcome to beeOS ", version)
     username = input("\n\n\n\n\nwrite your username:>")
 
+    #'user' home
     while run:
         choice = input(username + ":>")
 
+        #home functions
         if choice == "exit":
             run = False
 
         elif choice == "BOS $version":
             print("BeeOS version ", version)
+
+        #folders
+        elif choice == "!folders":
+            while folder:
+                folderschoice = input("folders/" + username + ":>")
+
+                if folderschoice == "exit":
+                    folder = False
+                
+        
+        #settings 
+        elif choice == "!settings":
+            while settings:
+                settingchoice = input("settings/" + username + ":>")
+                
+                if settingchoice == "exit":
+                    settings = False
+
+                elif settingchoice == "sys updates":
+                    print("there's no System updates yet")
+
+                elif settingchoice == "os updates":
+                    print("there's no OS updates yet")
+
 
     
 main()
